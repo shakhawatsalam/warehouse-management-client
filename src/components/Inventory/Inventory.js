74b2eedbe64ed7cecf2a61d466deb7c022edeafb
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-06 23:41:48 +0600

    restock button is done

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,7 +4,7 @@ import './Inventory.css';
 
 const Inventory = () => {
     const { id } = useParams();
-    const [data, setData] = useState({quantity:''});
+    const [data, setData] = useState({ quantity: '' });
 
 
     useEffect(() => {
@@ -14,46 +14,81 @@ const Inventory = () => {
             .then(data => setData(data));
         // console.log(data);
     }, []);
+    //DELIVER BUTTON 
     const number = parseInt(data.quantity);
     const { quantity, ...rest } = data;
-    const handleDeliverButton = (event) => {
-        // event.preventDefault();
+    const handleDeliverButton = () => {
         const newQuantity = number - 1;
         const data = {
-            quantity : newQuantity
+            quantity: newQuantity
         }
         const url = `http://localhost:5000/items/${id}`
         fetch(url, {
             method: 'PUT',
             headers: {
-                'content-type' : 'application/json'
+                'content-type': 'application/json'
             },
             body: JSON.stringify(data)
         })
             .then(res => res.json())
             .then(data => {
                 setData({ quantity: newQuantity, ...rest });
-        })
+            })
     }
-    return (
-        <div className='container'>
-            <div className='inventory-details d-flex justify-content-between align-items-center mt-5'>
-                <div className='img'>
-                    <img src={data.img} alt="" />
-                </div>
-                <div className='details'>
-                    <h3>Name : {data.name}</h3>
-                    <p>{data.description}</p>
-                    <h4>Price: {data.price}</h4>
-                    <h4>Quantity: {data.quantity}</h4>
-                    <h4>Quantity: {data.supplier_name}</h4>
-                    {data.quantity > 0 ?
-                        <button onClick={handleDeliverButton} className='btn btn-secondary mt-'>Delivered</button> :
-                        <button onClick={handleDeliverButton} className='btn btn-danger mt-' disabled>Stock Out</button>
-                    }
-                </div>
-            </div>
 
+    // RESTOCK FROM 
+    const oldQuantity = parseInt(data.quantity);
+    const handleStock = event => {
+        event.preventDefault();
+        let inputNumber = parseInt(event.target.restock.value);
+
+        if (!isNaN(inputNumber)) {
+            const quantity = inputNumber + oldQuantity;
+            const data = { quantity };
+            const url = `http://localhost:5000/items/${id}`
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('success', data);
+                    setData({ quantity: quantity, ...rest });
+                    event.target.reset('')
+                })
+        }
+    }
+
+    return (
+        <div>
+            <div className='container'>
+                <div className='inventory-details d-flex justify-content-between align-items-center mt-5'>
+                    <div className='img'>
+                        <img src={data?.img} alt="" />
+                    </div>
+                    <div className='details'>
+                        <h3>Name : {data?.name}</h3>
+                        <p>{data?.description}</p>
+                        <h4>Price: {data?.price}</h4>
+                        <h4>Quantity: {data?.quantity}</h4>
+                        <h4>Quantity: {data?.supplier_name}</h4>
+                        {data.quantity > 0 ?
+                            <button onClick={handleDeliverButton} className='btn btn-secondary mt-'>Delivered</button> :
+                            <button onClick={handleDeliverButton} className='btn btn-danger mt-' disabled>Stock Out</button>
+                        }
+
+                    </div>
+                </div>
+
+            </div>
+            <form onSubmit={handleStock} className='d-flex align-items-center mt-5 justify-content-center flex-column'>
+                <label htmlFor="restock">Restock</label>
+                <input className='' type="text" name="restock" id="" />
+                <input className='mt-3 btn btn-primary' type="submit" value="Restock" />
+            </form>
         </div>
     );
 };
